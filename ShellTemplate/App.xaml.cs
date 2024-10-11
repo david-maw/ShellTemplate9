f4962ca4547805954dd271065c589975faa52abf
David Maw <david@maw.name>
2024-10-10 18:30:27 -0700

    Add project files.

---
+++ ShellTemplate/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ShellTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+#if WINDOWS
+            window.Height = 600;
+            window.Width = 400;
+#endif          
+            return window;
+        }
+    }
+}
